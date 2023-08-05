1bd6b9589758459ebae59c3c3ebe66cb19f0952d
Utsav Kaushish <60324120+uak3@users.noreply.github.com>
2023-08-05 09:24:47 -0400

    Create stg_dim_customer_integrations.sql

---
+++ models/dim_customer_integrations/stg_dim_customer_integrations.sql
@@ -0,0 +1,29 @@
+--In this model, we create a "staging" table that we can supplement with other data to build the dimension table. 
+--It's incremental so that we can limit the number of rows we have to add in on every run. 
+
+{{ config(
+    materialized='incremental', 
+    schema = "stg", 
+    unique_key = "id"
+    ) 
+}}
+
+SELECT
+  ci.id, 
+  ci.created, 
+  ci.customer_id, 
+  cust.name AS customer_name, 
+  ig.name AS integration_name,
+  ig.category AS integration_category 
+FROM prod.customer_integrations AS ci 
+  --Everything from "prod" would be the untransformed data directly from the product, synced via fivetran
+  --customer_integrations would be a table with one row per integration
+LEFT JOIN prod.customers AS cust --one row per customer with descriptive columns
+	ON ci.customer_id = cust.id
+LEFT JOIN prod.integrations AS ig --one row per integration, from https://www.vanta.com/integrations
+	ON ci.integration_id = ig.id 
+  
+--incremental model: only load in new customer integrations, i.e. ones created after table was last updated
+{% if is_incremental() %}
+WHERE ci.created > (SELECT MAX(created) FROM {{ this }})
+{% endif %}
